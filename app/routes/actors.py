df3b052642413e828a2ea6d01a17cff710912e13
yuting <yuting@cosi107.cosi107a.org>
2025-02-25 23:28:41 -0500

    p1

--- app/routes/actors.py
+++ app/routes/actors.py
@@ -11,7 +11,11 @@ def view_all_actors():
     # >>>> TODO 1: Write a query that fetches all actors from `People` <<<<
     #              The query should retrieve `name`, `nationality`, `dob`, and `gender`.
 
-    query = """ """
+    query = """SElECT DISTINCT p.name, p.nationality, p.dob, p.gender
+    	       FROM People p
+    	       JOIN Role r ON p.id = r.pid
+    	       WHERE r.role_name = 'Actor';
+    	    """
 
     with Database() as db:
         actors = db.execute(query=query)
